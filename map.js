ecd87858f87ca2e5c35e61308cf8531ad492ae48
uhjavier <javierlam200518@gmail.com>
2023-02-08 14:39:53 +0800

    added location map for stores we have

---
+++ map.js
@@ -0,0 +1,14 @@
+var stores = [  {    name: "Store 1",    lat: 1.3521,    lng: 103.8198  },  {    name: "Store 2",    lat: 1.2904,    lng: 103.8520  }];
+
+var map = L.map("map").setView([1.3521, 103.8198], 13);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+}).addTo(map);
+
+for (var i = 0; i < stores.length; i++) {
+  var store = stores[i];
+  L.marker([store.lat, store.lng]).addTo(map)
+    .bindPopup(store.name)
+    .openPopup();
+}
