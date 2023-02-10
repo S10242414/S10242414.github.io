6c5a90326c0b16e96eeb4f904424217e62acbea6
uhjavier <javierlam200518@gmail.com>
2023-02-10 20:25:17 +0800

    testing signup login again, redid

--- loginsignup/signup.js
+++ loginsignup/signup.js
@@ -1,94 +1,35 @@
-var Username = [];    
-var Email = [];
-var Password =[];
-var ConfirmPassword = [];
-$(document).ready(function () {
-    const APIKEY = "63d565e83bc6b255ed0c43c7";
-    getdata();
-    checkdb();
-    
-    $("#signupbtn").on("click", function (e) 
-    {
-        e.preventDefault();
-        var Username = $("#username").val();    
-        var Email = $("#email").val();
-        var Password = $("#password").val();
-        var ConfirmPassword = $("#confirm-password").val();
-        let jsondata = {
-            "username": Username,
-            "email": Email,
-            "password": Password
-        };
-        let settings ={
-            "async": true,
-            "crossDomain": true,
-            "url": "https://nuirimaa-56ee.restdb.io/rest/users",
-            "method": "POST", 
-            "headers":{
-                "content-type": "application/json",
-                "x-apikey": APIKEY,
-                "cache-control": "no-cache"
-            },
-            "processData": false,
-            "data": JSON.stringify(jsondata),
-        };
-        $.ajax(settings).done(function (response) 
-        {
-            console.log(response);
-            $("#sign_up_submit").prop( "disabled", false);
-        });
-        if (checkdatabase() == false)
-        {
-            alert("Sign in failed.");
-            console.log(authenticate);
-        }
-        else
-        {
-            alert("Sign in Successfully.");
-            console.log(authenticate);  
-            window.location.href = "index.html";
-        }
-    });
-
-function getdata(){
-    let settings = 
-    {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://members-5211.restdb.io/rest/member-data",
-        "method": "GET",
-        "headers": {
-            "content-type": "application/json",
+const APIKEY = "63e633c0478852088da6801b"
+$(document).ready(function() {
+    $("#signupbtn").click(function(e) {
+      e.preventDefault();
+      var email = $("#email").val();
+      var username = $("#username").val();
+      var password = $("#password").val();
+      var confirmPassword = $("#confirm-password").val();
+  
+      if (password === confirmPassword) {
+        $.ajax({
+          type: "POST",
+          url: "https://nuirimaa-56ee.restdb.io/rest/users",
+          headers: {
             "x-apikey": APIKEY,
-            "cache-control": "no-cache"
-        }
-    }
-    $.ajax(settings).done(function (response) 
-    {
-        console.log(response);
-        for (let i = 0; i < response.length; i++) 
-        {
-            username.push(response[i].username);
-            password.push(response[i].password); 
-            email.push(response[i].email);
-        }
-
-    })
-}
-function checkdb(){
-        if (Email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
-            if(Password == ConfirmPassword){
-                alert("Signed in succesfully!")
-            }
-            else{
-                alert("Passwords do not match.");
-                return false;
-            }
-        } 
-        else {
-            alert("Invalid email address.");
-            return false;
-        }
-    }
-});
-
+            "content-type": "application/json"
+          },
+          data: JSON.stringify({
+            username: username,
+            email: email,
+            password: password
+          }),
+          success: function(data) {
+            console.log("Successfully signed up!");
+          },
+          error: function(error) {
+            console.log("Error signing up: " + error);
+          }
+        });
+      } else {
+        console.log("Password and confirm password do not match");
+      }
+    });
+  });
+  
